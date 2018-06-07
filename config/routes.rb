331a075af51d79b6f28d8a2a60d7d7f2c64fbe43
MBcdailey <chad.dailey@ministrybrands.com>
2018-06-07 11:36:04 -0400

    Added a projects route to display specific projects. Added a name attribute to the new project form. added links to the list of projects on the dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
   post '/new' => 'dashboard#new'
+  get '/projects/:id' => 'dashboard#projects'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
